273607598f1e76b4f97c6f3bc2d2b39afa50b41f
Ian Andrewson <ianandrewson@gmail.com>
2020-01-02 11:06:37 -0800

    test written for user login route, ready to TDD route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const User = require('../lib/models/User.js');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -21,6 +22,24 @@ describe('app routes', () => {
   it('should be able to post a new user', () => {
     return request(app)
       .post('/api/v1/auth/signup')
+      .send({ email: 'test@test.test', password: 'password' })
+      .then(res => {
+        expect(res.header['set-cookie'][0]).toEqual(expect.stringContaining('session='));
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          email: 'test@test.test',
+          __v: 0
+        });
+      });
+  });
+  it('can login a user with correct email and password', async() => {
+    const user = await User.create({
+      email: 'test@test.test',
+      password: 'password'
+    });
+
+    return request(app)
+      .post('/api/v1/auth/login')
       .send({ email: 'test@test.test', password: 'password' })
       .then(res => {
         expect(res.header['set-cookie'][0]).toEqual(expect.stringContaining('session='));
